0c52ed965616317c0451c618ec964e811eb40341
RMacy <ryan@hackery.io>
2018-05-17 13:07:50 -0400

    add webrtc and tensorflow
    Signed-off-by: RMacy <ryan@hackery.io>

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,67 @@
-import Container from '../components/container'
+import React, { Component } from "react";
+import Container from "../components/container";
 
-const Index = () => (
-  <Container>
-    <h3>Spit me a beat</h3>
-  </Container>
-)
+import * as posenet from "@tensorflow-models/posenet";
 
-export default Index
+class Index extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      constraints: {
+        audio: false,
+        video: true
+      },
+      scaleFactor: 0.5,
+      flipHorizontal: true,
+      outputStride: 16
+    };
+
+    this.video = React.createRef();
+  }
+
+  componentDidMount() {
+    // setup webrtc
+    navigator.mediaDevices
+      .getUserMedia(this.state.constraints)
+      .then(this.handleWebRTCSuccess)
+      .catch(this.handleWebRTCError);
+
+    posenet.load().then(net => {
+      setInterval(async () => {
+        let features = await net.estimateSinglePose(
+          this.video.current,
+          this.state.scaleFactor,
+          this.state.flipHorizontal,
+          this.state.outputStride
+        );
+
+        console.log(features);
+      });
+    });
+  }
+
+  handleWebRTCSuccess = stream => {
+    this.video.current.srcObject = stream;
+  };
+
+  handleWebRTCError = error => {
+    console.log(error);
+  };
+
+  render() {
+    return (
+      <Container>
+        <video
+          autoPlay
+          playsInline
+          ref={this.video}
+          height="480px"
+          width="480px"
+        />
+      </Container>
+    );
+  }
+}
+
+export default Index;
